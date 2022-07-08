286f3cf9f7780ee59a21e392ca9a3e8a80d8ed27
BlueStaxks <108381028+BlueStaxks@users.noreply.github.com>
2022-07-09 02:38:40 +0900

    week1 upload

---
+++ Week1/henry/1-3.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool comp(string& a, string& b)
+{
+	//printf("*");
+	int i = 0, j = 0, t1 = 0, t2 = 0;
+	char t;
+	string s1 = "", s2 = "";
+	while (!('0' <= a[i] && a[i] <= '9')) //헤드 파트
+	{
+		t = a[i];
+		if ('A' <= t && t <= 'Z')
+			t += 32;
+		s1 += t;
+		i++;
+	}
+	while (!('0' <= b[j] && b[j] <= '9'))
+	{
+		t = b[j];
+		if ('A' <= t && t <= 'Z')
+			t += 32;
+		s2 += t;
+		j++;
+	}
+	if (s1 != s2)
+		return s1 < s2;
+	while (('0' <= a[i] && a[i] <= '9')) //숫자 파트
+	{
+		t1 *= 10;
+		t1 += a[i] - '0';
+		i++;
+	}
+	while (('0' <= b[j] && b[j] <= '9'))
+	{
+		t2 *= 10;
+		t2 += b[j] - '0';
+		j++;
+	}
+	return t1 <= t2;
+}
+vector<string> solution(vector<string> files)
+{
+	string t;
+	for (int i = 0; i < files.size(); ++i) //버블 정렬
+		for (int j = 0; j < files.size() - 1 - i; ++j)
+			if (!comp(files[j], files[j + 1]))
+			{
+				t = files[j];
+				files[j] = files[j + 1];
+				files[j + 1] = t;
+			}
+	//sort(files.begin(), files.end(), comp);
+	return files;
+}
