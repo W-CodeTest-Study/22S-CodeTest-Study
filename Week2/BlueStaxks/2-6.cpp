006ed7c1f83427d689e565569392b92d35d12652
BlueStaxks <108381028+BlueStaxks@users.noreply.github.com>
2022-07-18 13:44:48 +0900

    Update 2-6.cpp

--- Week2/BlueStaxks/2-6.cpp
+++ Week2/BlueStaxks/2-6.cpp
@@ -13,9 +13,9 @@ int solution(vector<vector<string>> clothes)
         v.push_back(clothes[i][1]);
         m[clothes[i][1]]++; //옷 종류당 몇개인지 체크
     }
-    sort(v.begin(), v.end()); //18번 줄의 중복 제거를 위해 sort가 필요함
+    sort(v.begin(), v.end()); //17번 줄의 중복 제거를 위해 sort가 필요함
     v.erase(unique(v.begin(), v.end()), v.end()); //v는 옷 종류를 담는 배열
     for (i = 0; i < v.size(); ++i)
         r *= (m[v[i]] + 1); //경우의 수 연산
-    return r - 1; //다 벗은 경우 제외 + clothes가 하나도 없을 경우 자동으로 0 리턴(r이 처음엔 1이니까)
+    return r - 1; //다 벗은 경우 제외, clothes가 하나도 없을 경우 자동으로 0 리턴(r이 처음엔 1이니까)
 }
