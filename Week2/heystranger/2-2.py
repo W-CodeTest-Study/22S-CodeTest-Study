7d37dab4d95172f5e649d3205b808b3c028c7289
hyejin <hjhur004@gmail.com>
2022-07-18 11:48:48 +0900

    2-2 올바른 괄호 [solve]

--- Week2/heystranger/2-2.py
+++ Week2/heystranger/2-2.py
@@ -1,7 +1,7 @@
 def solution(s):
     answer = True
-    left = 0  # ( 갯수를 left라고 놓음
-    right = 0  # ) 갯수를 right라고 놓음
+    left = 0  # '(' 갯수를 left라고 놓음
+    right = 0  # ')' 갯수를 right라고 놓음
 
     for i in range(len(s)):
         if s[i] == '(':
